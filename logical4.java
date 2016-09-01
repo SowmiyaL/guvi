2f03acfcdc91f722c24499f16509b1cb54231f52
SowmiyaL <sowmiyaait20@gmail.com>
2016-09-02 00:03:07 +0530

    Add files via upload

---
+++ logical4.java
@@ -0,0 +1,43 @@
+package logicalbuling;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class logical4 {
+
+
+public static void main(String[] args) 
+{
+        Scanner s = new Scanner(System.in);
+       System.out.print("enter the  n number");
+       int n = s.nextInt();
+        int[] a = new int[n];
+
+          for(int i= 0; i < n; i++)
+          {
+        	  a[i] = s.nextInt();
+        	  
+          }
+          Arrays.sort(a);
+          for(int i=0;i<n;i++)
+          {
+        	  for(int j=i;j<n;j++)
+        	  {
+        		  if(a[i]==a[j+1]) 
+        		  {
+        			 a[j] = '$';
+        		  }
+        		  
+        	  }
+          }
+         
+          for(int k=n-1;k>=0;k--)
+          {
+        	  System.out.println(a[k]);
+          }
+
+
+}
+}
+
+
